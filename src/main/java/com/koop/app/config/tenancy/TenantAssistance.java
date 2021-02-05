9b3552b676d2e0e85165c8ce22ed2e609019b159
mertmr <mertmr@gmail.com>
2021-02-05 11:17:02 +0300

    tenant bir nedenden ötürü saçmalıyor, log attım

--- src/main/java/com/koop/app/config/tenancy/TenantAssistance.java
+++ src/main/java/com/koop/app/config/tenancy/TenantAssistance.java
@@ -1,9 +1,13 @@
 package com.koop.app.config.tenancy;
 
 import com.koop.app.security.CurrentUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public final class TenantAssistance {
+
+    private static final Logger log = LoggerFactory.getLogger(TenantAssistance.class);
 
     private TenantAssistance() {
     }
@@ -17,6 +21,8 @@ public final class TenantAssistance {
                 Long tenant = authentication.getTenant();
                 if (tenant != null)
                     return tenant;
+
+                log.error(tenant.toString());
             }
         } else {
             return 0L;
