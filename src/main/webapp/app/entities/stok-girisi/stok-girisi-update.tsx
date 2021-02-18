49a6fe9f8e9e49ba3b16d866d01c9720e163d134
mertmr <mertmr@gmail.com>
2021-02-18 21:28:35 +0300

    fireyi sadece mali birim yapsın

--- src/main/webapp/app/entities/stok-girisi/stok-girisi-update.tsx
+++ src/main/webapp/app/entities/stok-girisi/stok-girisi-update.tsx
@@ -214,7 +214,7 @@ export const StokGirisiUpdate = (props: IStokGirisiUpdateProps) => {
                   value={(!isNew && stokGirisiEntity.stokHareketiTipi) || 'STOK_GIRISI'}
                 >
                   <option value="STOK_GIRISI">{translate('koopApp.StokHareketiTipi.STOK_GIRISI')}</option>
-                  <option value="FIRE">{translate('koopApp.StokHareketiTipi.FIRE')}</option>
+                  <option style={isAdmin ? {} : { display: 'none' }} value="FIRE">{translate('koopApp.StokHareketiTipi.FIRE')}</option>
                   <option style={isAdmin ? {} : { display: 'none' }} value="STOK_DUZELTME">
                     {translate('koopApp.StokHareketiTipi.STOK_DUZELTME')}
                   </option>
