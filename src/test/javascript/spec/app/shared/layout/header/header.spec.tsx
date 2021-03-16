603e0700d267e6b62acd24c4a87d370e3265e0e7
mertmr <mertmr@gmail.com>
2021-03-16 10:07:24 +0300

    umarım çözülecek bu sefer

--- src/test/javascript/spec/app/shared/layout/header/header.spec.tsx
+++ src/test/javascript/spec/app/shared/layout/header/header.spec.tsx
@@ -55,7 +55,7 @@ describe('Header', () => {
   it('Renders a Header component in dev profile with LoadingBar, Navbar, Nav and dev ribbon.', () => {
     const component = wrapper();
     // the created snapshot must be committed to source control
-   // expect(component).toMatchSnapshot();
+    expect(component).toMatchSnapshot();
     expect(component.find(LoadingBar).length).toEqual(1);
     const navbar = component.find(Navbar);
     expect(navbar.length).toEqual(1);
